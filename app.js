03b07a80f09b5c7f760b34d2558203c045295967
Toroi0610 <toroi0610@gmail.com>
2020-04-28 23:46:53 +0900

    show messages on browser

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const io = require("socket.io")(http);
 io.on("connection", function(socket){
     socket.on("message", function(msg){
         console.log("message: " + msg);
+        io.emit("message", msg);
     });
 });
 
